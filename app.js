ccc45a818b88d7dcb7a0b1627e843c176d3e62f8
CJMoses1318 <cjmoses1318@gmail.com>
2025-12-09 12:46:58 -0800

    done before final details

--- app.js
+++ app.js
@@ -1,35 +1,62 @@
 // template_7pe8ze9
-// service_klzb15v 
+// service_klzb15v
 // 4_zbg61EHOmowMpIH
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
-function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
-    loading.classList += " modal__overlay--visible";
-    emailjs
-    .sendForm(
-        'service_klzb15v',
-        'template_7pe8ze9',
-        event.target,
-        '4_zbg61EHOmowMpIH'
-    ).then(() => {
-        loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay-visible";
-    }).catch (() => {
-        loading.classList.remove("modal__overlay--visible");
-        alert(
-            "The email service is temporarily unavailable. Please contact me directly at cjmoses1318@gmail.com."
-        );
-    })
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    // Added rotate after tutorial
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+  }
 }
 
-let isModalOpen = false;
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_klzb15v",
+      "template_7pe8ze9",
+      event.target,
+      "4_zbg61EHOmowMpIH"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+      );
+    });
+}
+
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open")
-    }
-    isModalOpen = true;
-    document.body.classList =+ " modal--open";
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
